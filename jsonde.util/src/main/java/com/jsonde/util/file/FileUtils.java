fbd31b23b89d92807cd30db415403ab77781769f
gabriele <gabriele@192.168.1.101>
2017-08-05 17:44:25 +0200

    Commit del 5 agosto 2017 alle ore 17,44

--- jsonde.util/src/main/java/com/jsonde/util/file/FileUtils.java
+++ jsonde.util/src/main/java/com/jsonde/util/file/FileUtils.java
@@ -2,6 +2,7 @@ package com.jsonde.util.file;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -21,13 +22,28 @@ public class FileUtils {
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
     public static final String FILE_SEPARATOR = System.getProperty("file.separator");
     public static final String PATH_SEPARATOR = System.getProperty("path.separator");
-    public static final String USER_HOME = System.getProperty(user());
+    private String user = getUser();
+    public static final String USER_HOME = System.getProperty(user);
+
+    public static boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
+    }
     
     /**
 	 * Legge da file il nome utente di MySQL
 	 * @return
 	 */
-	private static String user() {
+	private static String getUser() {
 		String s = null;
 		try {
 			PrintStream output = new PrintStream(new File("user.txt"));
@@ -50,21 +66,33 @@ public class FileUtils {
 		}
 		return s;
 	}
-	
-    public static boolean deleteDirectory(File path) {
-        if (path.exists()) {
-            File[] files = path.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                if (files[i].isDirectory()) {
-                    deleteDirectory(files[i]);
-                } else {
-                    files[i].delete();
-                }
-            }
-        }
-        return (path.delete());
-    }
-
+    /**
+	 * Legge da file il nome utente di MySQL
+	 * @return
+	 */
+	public static String user() {
+		String s = null;
+		try {
+			PrintStream output = new PrintStream(new File("user.txt"));
+			output.println("root");
+			output.flush();
+			output.close();
+		} catch (FileNotFoundException e1) {
+			JOptionPane.showMessageDialog (
+				null , "Problemi di lettura da file user"
+			);
+		}
+		try {
+			@SuppressWarnings("resource")
+			BufferedReader buffer = new BufferedReader(new FileReader("user.txt"));
+			s = buffer.readLine();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog (
+				null , "Problemi di lettura da file user"
+			);
+		}
+		return s;
+	}
     public static boolean createFile(File file) {
 
         if (!file.exists()) {
